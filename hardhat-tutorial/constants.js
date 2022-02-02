61760f516bbba779fb7af18f8180aebb2827cfed
owiniose <98717429+owiniose@users.noreply.github.com>
2022-02-02 17:11:38 +0100

    Add files via upload

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x8AF78b4714Ca7235aE6E7FdeDCEB517c612d25BD";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
