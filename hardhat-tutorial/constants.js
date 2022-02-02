9cb9070fbc7b484b5092a9c898823afb31f86b6e
owiniose <98717429+owiniose@users.noreply.github.com>
2022-02-02 17:12:04 +0100

    Delete hardhat-tutorial directory

--- hardhat-tutorial/constants.js
+++
@@ -1,5 +0,0 @@
-// Replace the value with your NFT contract address
-const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-  "0x8AF78b4714Ca7235aE6E7FdeDCEB517c612d25BD";
-
-module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
